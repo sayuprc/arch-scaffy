29aff39313bc90642f0979fa222e788b7e95142c
sayuprc <bepdielibe+github@gmail.com>
2025-12-07 00:40:02 +0900

    Add determination methods

--- src/Config/Feature/Kind.php
+++ src/Config/Feature/Kind.php
@@ -6,8 +6,28 @@ namespace ArchScaffy\Config\Feature;
 
 enum Kind: string
 {
-    case Class = 'class';
+    case ClassKind = 'class';
     case Interface = 'interface';
     case Trait = 'trait';
     case Enum = 'enum';
+
+    public function isClass(): bool
+    {
+        return $this === self::ClassKind;
+    }
+
+    public function isInterface(): bool
+    {
+        return $this === self::Interface;
+    }
+
+    public function isTrait(): bool
+    {
+        return $this === self::Trait;
+    }
+
+    public function isEnum(): bool
+    {
+        return $this === self::Enum;
+    }
 }
