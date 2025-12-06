d5a5fec5594ee7357335521ca9e6989fce592d60
sayuprc <bepdielibe+github@gmail.com>
2025-12-06 22:18:18 +0900

    Add YamlParser

---
+++ src/YamlParser/YamlParser.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArchScaffy\YamlParser;
+
+use Symfony\Component\Yaml\Yaml;
+use UnexpectedValueException;
+
+class YamlParser implements YamlParserInterface
+{
+    public function parseFile(string $file): array
+    {
+        $array = Yaml::parseFile($file);
+
+        if (! is_array($array)) {
+            throw new UnexpectedValueException(
+                sprintf(
+                    'The parsing result of the YAML file "%s" was expected to be an array, but it is an %s.',
+                    $file,
+                    get_debug_type($array)
+                )
+            );
+        }
+
+        return $array;
+    }
+}
